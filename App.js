8ef35ed388bbc26d39dd022be1808969b61e5505
victoryuchenna <87183426+victoryuchenna@users.noreply.github.com>
2022-02-03 22:15:48 +0000

    test

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import * as Location from 'expo-location';
 
 export default function App() {
